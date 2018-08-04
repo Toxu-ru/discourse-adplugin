0cc80a987b7c56c2c520a7413570887b80ea2f81
Evg <budo@narod.ru>
2018-08-04 16:32:05 +0300

    Update ads_controller.rb

--- app/controllers/discourse_ads/ads_controller.rb
+++ app/controllers/discourse_ads/ads_controller.rb
@@ -3,8 +3,20 @@ module DiscourseAds
  
    skip_before_action :check_xhr, only: [:index]
 
- # def index
- # end
+   def index
+
+      @ads_txt = agent_id_info
+      render :index, content_type: 'text/plain'
+ 
+   end
+ 
+ 
+   def agent_id_info
+ 
+     @pub_id =  '1122334455' 
+      
+   end
+ 
 
     end
 end
